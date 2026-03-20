7e698e0a8f217ab5c366e6f77d89eafca70096a5
Rio Fujita <rio_github@rio.st>
2026-03-20 10:24:26 +0900

    initial commit

---
+++ Casks/solixmenu.rb
@@ -0,0 +1,11 @@
+cask "solixmenu" do
+  version "1.0.9"
+  sha256 "9213c71750ac89719169253a50bbc98c1a61d1181402b18ee8089981bc58b87c"
+
+  url "https://github.com/rioriost/solixmenu/releases/download/v1.0.9/SolixMenu-v1.0.9.zip"
+  name "SolixMenu"
+  desc "Lightweight macOS menu bar app for monitoring Anker Solix devices"
+  homepage "https://github.com/rioriost/solixmenu"
+
+  app "SolixMenu.app"
+end
