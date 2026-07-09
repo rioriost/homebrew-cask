af1b1d8b2d8e6d84af39e5f30c2a579a142e0ab5
Rio Fujita <rio_github@rio.st>
2026-07-09 11:27:19 +0900

    Add git-labeler cask

---
+++ Casks/git-labeler.rb
@@ -0,0 +1,30 @@
+cask "git-labeler" do
+  version "0.1.0"
+  sha256 "2dcb435e47c9709ca84ede867c41ee763fdc1c7749db45440c6a30120f6c41fa"
+
+  url "https://github.com/rioriost/git-labeler/releases/download/v#{version}/git-labeler-#{version}-darwin-arm64.pkg"
+  name "git-labeler"
+  desc "Apply Finder tags to git repositories based on git status"
+  homepage "https://github.com/rioriost/git-labeler"
+
+  depends_on arch: :arm64
+  depends_on macos: :ventura
+
+  pkg "git-labeler-#{version}-darwin-arm64.pkg"
+
+  uninstall launchctl: "st.rio.git-labeler",
+            pkgutil:   "st.rio.git-labeler.pkg",
+            delete:    [
+              "/opt/homebrew/bin/git-labeler",
+              "/opt/homebrew/share/doc/git-labeler",
+              "/opt/homebrew/share/git-labeler",
+            ]
+
+  caveats <<~EOS
+    Configure one or more parent directories:
+      git-labeler config add ~/Git_Managed
+
+    Install and start the LaunchAgent:
+      /opt/homebrew/share/git-labeler/scripts/install-launchagent.sh
+  EOS
+end
