13c80aaf23f59819fbf7d9c81bf6d0390dc8a028
Rio Fujita <rio_github@rio.st>
2026-09-21 12:11:01 +0900

    Update AzCopy Mac UI to notarized 0.2.2

--- Casks/azcopy-mac-ui.rb
+++ Casks/azcopy-mac-ui.rb
@@ -1,6 +1,6 @@
 cask "azcopy-mac-ui" do
-  version "0.2.1"
-  sha256 "deee0af386acae1ab9c4cd03481000fc71adea3d4900ca0c1218079f1b015d12"
+  version "0.2.2"
+  sha256 "612f9f78ba9c5b932c5054744de7cd2e46bf32a46809a83be1284a8e7c32c3df"
 
   url "https://github.com/rioriost/azcopy-mac-ui/releases/download/v#{version}/azcopy-mac-ui-#{version}-macos-arm64.zip"
   name "AzCopy Mac UI"
@@ -8,8 +8,8 @@ cask "azcopy-mac-ui" do
   homepage "https://github.com/rioriost/azcopy-mac-ui"
 
   depends_on arch: :arm64
-  depends_on macos: :sonoma
   depends_on formula: "azcopy"
+  depends_on macos: :sonoma
 
   app "AzCopy Mac UI.app"
 
